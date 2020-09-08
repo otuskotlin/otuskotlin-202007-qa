668e5b0f47175c47e1773694116330fc108bdcb0
Elena R. Nagornova <nagornova@yamoney.ru>
2020-09-08 21:10:17 +0300

    hw1. Добавила новые модули qa-common и qa-transport-common

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,7 @@
+plugins {
+    kotlin("jvm") apply false
+    kotlin("multiplatform") apply false
+}
+
 group = "ru.otus.otuskotlin.qa"
 version = "0.0.1"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "otuskotlin-202007-qa-eg"
+
+
+include(":qa-common")
+include(":qa-transport-common")
+
+
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+    }
+}
+
+
